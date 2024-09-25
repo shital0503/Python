6b69ab9a2d5c46a88b2f8bfebd2e355a4789120a
Shital Bhosale <157453803+shital0503@users.noreply.github.com>
2024-09-25 10:16:10 +0530

    Add files via upload

---
+++ slip2.py
@@ -0,0 +1,11 @@
+d={'akanksha':1,'tanuja':5,'prasiddhi':3}
+print("the given Dictionary:",d)
+check_key=input("enter key to check:")
+check_value=input("enter value to check:")
+if check_key in d:
+    print(check_key,"is present" in d.pop(check_key))
+    d[check_key]=check_value
+else:
+    print(check_key,"is not present,")
+    d[check_key]=check_value
+    print("updateddictionary:",d)
